a193175c1142a2e537bd2df8ece50f4b31afedf3
100ngramjit <sangramjitdutta@gmail.com>
2023-03-26 19:47:19 +0530

    add to card moved to context file

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -4,9 +4,20 @@ export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
+  const handleAddToCart = (product) => {
+    const itemIndex = cartItems.findIndex((item) => item.id === product.id);
+    if (itemIndex >= 0) {
+      const updatedCartItems = [...cartItems];
+      updatedCartItems[itemIndex].quantity += 1;
+      setCartItems(updatedCartItems);
+    } else {
+      const newCartItems = [...cartItems, { ...product, quantity: 1 }];
+      setCartItems(newCartItems);
+    }
+  };
 
   return (
-    <CartContext.Provider value={{ cartItems, setCartItems }}>
+    <CartContext.Provider value={{ cartItems, setCartItems, handleAddToCart }}>
       {children}
     </CartContext.Provider>
   );
